3580d5edd1232ae6decce1b21a860e2fee603b31
Ahmad Taha <ataha@netways.com>
2021-09-12 00:31:02 +0300

    ⚡️ basic HeroBanner implementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,47 @@
+import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styled from "styled-components";
+import { useWindowSize } from "../hooks/useWindowResize";
 
-const Title = styled.h1`
+const HeroBanner = styled(motion.div)`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MainText = styled.div.attrs((props: { width: number }) => ({
+  width: props.width || 240,
+}))`
   color: ${({ theme }) => theme.colors.white};
-  font-size: 15vw;
+  font-family: ${({ theme }) => theme.fontFamily.salsa};
+  font-size: ${({ width }) => `${width / 6}px`};
   text-align: center;
 `;
 
 const Home: NextPage = () => {
+  const { width } = useWindowSize();
+
   return (
-    <div>
+    <>
       <Head>
         <title>Ahmad Taha</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Title> Ahmad Taha</Title>
-    </div>
+      <HeroBanner
+        initial="hidden"
+        animate="visible"
+        transition={{ duration: 2 }}
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+        }}
+      >
+        <MainText width={width}>Ahmad Taha</MainText>
+      </HeroBanner>
+    </>
   );
 };
 
