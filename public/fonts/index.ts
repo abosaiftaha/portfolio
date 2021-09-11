fc02df0720be85aa6717112ca4b263f79d8d0d82
Ahmad Taha <ataha@netways.com>
2021-09-11 04:49:49 +0300

    🚧 add fonts

---
+++ public/fonts/index.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components';
+
+const FontGlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'Lato';
+    src: url('/fonts/Lato-Light.eot');
+    src: url('/fonts/Lato-Light.eot?#iefix') format('embedded-opentype'),
+        url('/fonts/Lato-Light.woff2') format('woff2'),
+        url('/fonts/Lato-Light.woff') format('woff'),
+        url('/fonts/Lato-Light.ttf') format('truetype'),
+        url('/fonts/Lato-Light.svg#Lato-Light') format('svg');
+    font-weight: 300;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Lato';
+    src: url('/fonts/Lato-Regular.eot');
+    src: url('/fonts/Lato-Regular.eot?#iefix') format('embedded-opentype'),
+        url('/fonts/Lato-Regular.woff2') format('woff2'),
+        url('/fonts/Lato-Regular.woff') format('woff'),
+        url('/fonts/Lato-Regular.ttf') format('truetype'),
+        url('/fonts/Lato-Regular.svg#Lato-Regular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Lato';
+    src: url('/fonts/Lato-Bold.eot');
+    src: url('/fonts/Lato-Bold.eot?#iefix') format('embedded-opentype'),
+        url('/fonts/Lato-Bold.woff2') format('woff2'),
+        url('/fonts/Lato-Bold.woff') format('woff'),
+        url('/fonts/Lato-Bold.ttf') format('truetype'),
+        url('/fonts/Lato-Bold.svg#Lato-Bold') format('svg');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Salsa BT';
+    src: url('SalsaBT-Regular.eot');
+    src: url('SalsaBT-Regular.eot?#iefix') format('embedded-opentype'),
+        url('SalsaBT-Regular.woff2') format('woff2'),
+        url('SalsaBT-Regular.woff') format('woff'),
+        url('SalsaBT-Regular.ttf') format('truetype'),
+        url('SalsaBT-Regular.svg#SalsaBT-Regular') format('svg');
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+
+`;
+
+export default FontGlobalStyle;
