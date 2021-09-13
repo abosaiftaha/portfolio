63227d5087e46139a5a3bda1398802f41c04b8bb
Ahmad Taha <ataha@netways.com>
2021-09-14 00:57:31 +0300

    💄 support light/dark themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
+import { useState } from "react";
 import {
   createGlobalStyle,
   DefaultTheme,
   ThemeProvider,
 } from "styled-components";
 import { Cursor } from "../components/cursor";
+import Nav from "../components/nav";
 import FontGlobalStyle from "../public/fonts";
 
 const GlobalStyle = createGlobalStyle`
@@ -27,57 +29,45 @@ const GlobalStyle = createGlobalStyle`
   }
   `;
 
-const theme: DefaultTheme = {
+const darkTheme: DefaultTheme = {
   colors: {
     blue: "#5AC1F4",
     yellow: "#FCD406",
     purple: "#BB7DFF",
     red: "#FE7171",
     black: "#000",
-    white: "#ffff",
+    white: "#fff",
   },
   fontFamily: {
     salsa: "Salsa BT",
     lato: "Lato",
   },
-  fontSize: {
-    xs: ".75rem",
-    sm: ".875rem",
-    tiny: ".875rem",
-    base: "1rem",
-    lg: "1.125rem",
-    xl: "1.25rem",
-    "2xl": "1.5rem",
-    "3xl": "1.875rem",
-    "4xl": "2.25rem",
-    "5xl": "3rem",
-    "6xl": "4rem",
-    "7xl": "5rem",
+};
+
+const lightTheme: DefaultTheme = {
+  colors: {
+    blue: "#5AC1F4",
+    yellow: "#FCD406",
+    purple: "#BB7DFF",
+    red: "#FE7171",
+    black: "#fff",
+    white: "#000",
   },
-  screens: {
-    sm: "640px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1280px",
-    "2xl": "1536px",
-  },
-  spacing: {
-    "1": "8px",
-    "2": "12px",
-    "3": "16px",
-    "4": "24px",
-    "5": "32px",
-    "6": "48px",
+  fontFamily: {
+    salsa: "Salsa BT",
+    lato: "Lato",
   },
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isDarkMode, setIsDarkMode] = useState(true);
   return (
     <>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
         <GlobalStyle />
         <FontGlobalStyle />
         <Cursor />
+        <Nav isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- styled.d.ts
+++ styled.d.ts
@@ -14,34 +14,5 @@ declare module "styled-components" {
       salsa: string;
       lato: string;
     };
-    fontSize: {
-      xs: string;
-      sm: string;
-      tiny: string;
-      base: string;
-      lg: string;
-      xl: string;
-      "2xl": string;
-      "3xl": string;
-      "4xl": string;
-      "5xl": string;
-      "6xl": string;
-      "7xl": string;
-    };
-    screens: {
-      sm: string;
-      md: string;
-      lg: string;
-      xl: string;
-      "2xl": string;
-    };
-    spacing: {
-      "1": string;
-      "2": string;
-      "3": string;
-      "4": string;
-      "5": string;
-      "6": string;
-    };
   }
 }
