bbe2f4dba004a4f6eebdd1713082af037a7191aa
Ahmad Taha <ataha@netways.com>
2021-09-15 03:55:13 +0300

    🍱 add FipSpiralSVG

---
+++ public/assets/FibSpiralSVG.tsx
@@ -0,0 +1,50 @@
+import * as React from "react"
+
+function FibSpiralSVG(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 1752.059 1085"
+      {...props}
+    >
+      <g data-name="Group 16" fill="none" stroke="#fff" strokeWidth={5}>
+        <path
+          data-name="Path 36"
+          d="M1272.85 828.383a31.765 31.765 0 01-31.765-31.765"
+        />
+        <path
+          data-name="Path 37"
+          d="M1336.379 764.853a63.529 63.529 0 01-63.529 63.529"
+        />
+        <path
+          data-name="Path 38"
+          d="M1241.085 669.559a95.294 95.294 0 0195.294 95.294"
+        />
+        <path
+          data-name="Path 39"
+          d="M1082.262 828.383a158.824 158.824 0 01158.823-158.824"
+        />
+        <path
+          data-name="Path 40"
+          d="M1336.38 1082.496c-140.346 0-254.118-113.773-254.118-254.118"
+        />
+        <path
+          data-name="Path 41"
+          d="M1749.32 669.559c0 228.061-184.88 412.941-412.941 412.941"
+        />
+        <path
+          data-name="Path 42"
+          d="M2.5 1082.5c0-596.468 483.533-1080 1080-1080"
+        />
+        <path
+          data-name="Path 43"
+          d="M1082.5 2.5c368.406 0 667.059 298.652 667.059 667.059"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default FibSpiralSVG
