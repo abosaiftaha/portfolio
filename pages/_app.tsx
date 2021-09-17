df7d4792f7b9a7bc5ed3e90deb13b4d04be02e3f
Ahmad Taha <ataha@netways.com>
2021-09-17 04:12:48 +0300

    💄 add grain to background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,14 +14,26 @@ const GlobalStyle = createGlobalStyle`
   body {
     padding: 0;
     margin: 0;
-    background-color: ${({ theme }) => theme.colors.black};
     overflow-x: hidden;
   }
-
+  
   body{
     cursor: none;
     isolation: isolate;
     padding: 0 70px;
+    background-color: ${({ theme }) => theme.colors.black};
+
+    &::after{
+      content: '';
+      background: url("/assets/grain.png") 0 0/150px auto;
+      opacity: 0.5;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      position: fixed;
+      z-index: -1;  
+    }
   }
 
   * {
