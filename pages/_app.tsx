00554f14e4594e2343148abe7d825ff0b2266a73
Ahmad Taha <ataha@netways.com>
2021-09-18 19:44:48 +0300

    🚚 move head to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { createGlobalStyle } from "styled-components";
 import { Cursor } from "../components/cursor";
 import Nav from "../components/nav";
@@ -41,6 +42,34 @@ const GlobalStyle = createGlobalStyle`
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ContextWrapper>
+      <Head>
+        <title>{"Ahmad's Portfolio"}</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <AppThemeProvider>
         <GlobalStyle />
         <FontGlobalStyle />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import type { NextPage } from "next";
-import Head from "next/head";
 import styled from "styled-components";
 import { Animations } from "../hooks/animations";
 import { useMousePosition } from "../hooks/useMousePosition";
@@ -46,34 +45,6 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <Head>
-        <title>{"Ahmad's Portfolio"}</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
       <HeroBanner
         initial="hidden"
         animate="visible"
