4495083ae2fdea816980b978df5f2aad56a1fadf
Ahmad Taha <ataha@netways.com>
2021-09-23 03:49:53 +0300

    💄 hero banner design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,79 +1,126 @@
 import { motion } from "framer-motion";
 import type { NextPage } from "next";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import { Animations } from "../hooks/animations";
 import { useMousePosition } from "../hooks/useMousePosition";
 import { useWindowSize } from "../hooks/useWindowResize";
-import GeekSVG from "../public/assets/GeekSVG";
+// import GeekSVG from "../public/assets/GeekSVG";
+import Tilt from "react-tilt";
 
-const HeroBanner = styled(motion.div)`
-  height: 100vh;
-  width: 100%;
+const HomeStyles = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  position: relative;
+
+  .hero-banner {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-end;
+      position: relative;
+      z-index: 1;
+
+      .main-text {
+        -webkit-text-stroke: 1px ${({ theme }) => theme.colors.white};
+        color: transparent;
+        font-family: ${({ theme }) => theme.fontFamily.salsa};
+        font-size: 10vw;
+        padding: 1vw 2vw 0 2vw;
+        text-align: center;
+        position: relative;
+        z-index: 2;
+        transition: all 0.2s ease-in-out;
+
+        &:hover {
+          color: ${({ theme }) => theme.colors.white};
+        }
+
+        &.hovered {
+          color: ${({ theme }) => theme.colors.white};
+        }
+      }
+
+      .secondary-text {
+        font-size: 1vw;
+        font-weight: 300;
+        padding: 0 2vw;
+
+        p {
+          margin: 0 0 5px 0;
+        }
+      }
+
+      &:hover {
+        &::after {
+          content: "";
+          position: absolute;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%, -50%);
+          width: 25vw;
+          height: 25vw;
+          background: url("/assets/emoji.png") no-repeat;
+          background-position: center;
+          background-size: contain;
+          transition: ease-in-out 0.5;
+          z-index: 2;
+        }
+      }
+    }
+  }
 `;
 
-const MainText = styled.div.attrs((props: { width: number }) => ({
-  width: props.width || 240,
-}))`
-  -webkit-text-stroke: 5px ${({ theme }) => theme.colors.white};
-  color: transparent;
-  font-family: ${({ theme }) => theme.fontFamily.salsa};
-  font-size: ${({ width }) => `${width / 6}px`};
-  text-align: center;
-  position: relative;
-  z-index: 2;
-`;
-
-const GeekStickerContainer = styled(motion.div)`
-  width: 180px;
-  height: 180px;
-  position: absolute;
-  right: -20px;
-  top: -50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+const variants = {
+  heroBanner: {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+    },
+  },
+};
 
 const Home: NextPage = () => {
   const { width } = useWindowSize();
-  const { transition } = Animations();
+  const { transition, textReveal } = Animations();
   const { hoverMainTitle } = useMousePosition();
+  const theme = useTheme();
 
   return (
-    <>
-      <HeroBanner
+    <HomeStyles>
+      <motion.div
+        className="hero-banner"
         initial="hidden"
         animate="visible"
-        transition={{ ...transition, duration: 1 }}
-        variants={{
-          hidden: {
-            y: "120%",
-          },
-          visible: {
-            y: "0%",
-          },
-        }}
+        transition={{ ...textReveal, duration: 2, delay: 4.2 }}
+        variants={variants.heroBanner}
       >
-        <MainText width={width}>
-          Ahmad Taha
-          <GeekStickerContainer
-            whileHover={{
-              scale: 1.1,
-              transition: {
-                duration: 1,
-                type: "spring",
-              },
-            }}
+        <Tilt options={{ max: 25 }}>
+          <motion.div
+            id="mainTitle"
+            className="content"
+            whileHover={{ color: theme.colors.white }}
           >
-            <GeekSVG />
-          </GeekStickerContainer>
-        </MainText>
-      </HeroBanner>
-    </>
+            <div className={`main-text ${hoverMainTitle && "hovered"}`}>
+              {"Ahmad Taha"}
+            </div>
+            <div className="secondary-text">
+              <p>Software Engineer</p>
+              <p>Front-end Geek</p>
+            </div>
+          </motion.div>
+        </Tilt>
+      </motion.div>
+    </HomeStyles>
   );
 };
 
