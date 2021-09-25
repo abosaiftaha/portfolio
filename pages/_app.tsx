271bf2bd22c42dc6467c3e8c766db82925e96749
Ahmad Taha <ataha@netways.com>
2021-09-26 01:28:20 +0300

    🎨 cleaning _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app";
-import Head from "next/head";
 import { createGlobalStyle } from "styled-components";
 import { Cursor } from "../components/cursor";
 import LoadingScreen from "../components/loading";
@@ -7,6 +6,7 @@ import Nav from "../components/nav";
 import AppThemeProvider from "../utils/themeProvider";
 import { ContextWrapper } from "../utils/contextProvider";
 import FontGlobalStyle from "../public/fonts";
+import AppHead from "../components/head";
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -41,8 +41,6 @@ const GlobalStyle = createGlobalStyle`
         right: 0;
         position: fixed;
         z-index: -1;  
-      z-index: -1;  
-        z-index: -1;  
       }
 
 
@@ -66,34 +64,7 @@ const GlobalStyle = createGlobalStyle`
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ContextWrapper>
-      <Head>
-        <title>{"Ahmad's Portfolio"}</title>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+      <AppHead />
       <AppThemeProvider>
         <GlobalStyle />
         <FontGlobalStyle />
